5010171e6c77d32aac145eac4688e143965c86bd
Irving Herrera <irving.herreramolina@gmail.com>
2018-11-01 15:20:50 -0600

    contexto de base de datos y mapeo de tabla categoria

---
+++ SistemaVenta/SistemaVenta.Data/DbContextSistema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SietemaVenta.Entity.Almacen;
+using SistemaVenta.Data.Mapping.Almacen;
+
+namespace SistemaVenta.Data
+{
+    public class DbContextSistema : DbContext
+    {
+        public DbSet<Categoria> Categoria { get; set; }
+
+        public DbContextSistema(DbContextOptions<DbContextSistema> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+        }
+    }
+}
---
+++ SistemaVenta/SistemaVenta.Data/Mapping/Almacen/CategoriaMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SietemaVenta.Entity.Almacen;
+
+namespace SistemaVenta.Data.Mapping.Almacen
+{
+    public class CategoriaMap : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.ToTable("categoria")
+                .HasKey(c => c.IdCategoria);
+            builder.Property(c => c.Nombre)
+                .HasMaxLength(50);
+            builder.Property(c => c.Descripcion)
+                .HasMaxLength(256);
+        }
+    }
+}
