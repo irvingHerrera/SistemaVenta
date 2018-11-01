8870b1d645541f608dc0c3935fd7d914a723fbb3
Irving Herrera <irving.herreramolina@gmail.com>
2018-11-01 14:18:49 -0600

    entidad categoria

---
+++ SistemaVenta/SietemaVenta.Entity/Almacen/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SietemaVenta.Entity.Almacen
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe tener más de 50 caracteres, ni menos de 3 caracteres. ")]
+        public string Nombre { get; set; }
+        [StringLength(256)]
+        public string Descripcion { get; set; }
+        public bool Condicion { get; set; }
+    }
+}
