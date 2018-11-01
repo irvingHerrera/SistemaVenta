739c25082f22b125d045bf1dad562624b7da0ba0
Irving Herrera <irving.herreramolina@gmail.com>
2018-11-01 15:43:57 -0600

    Inyeccion dependecia de contexto de base de datos, cadena de conexion

--- SistemaVenta/SistemaVenta.Web/Startup.cs
+++ SistemaVenta/SistemaVenta.Web/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SistemaVenta.Data;
 
 namespace SistemaVenta.Web
 {
@@ -26,6 +28,8 @@ namespace SistemaVenta.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<DbContextSistema>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("Conexion")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
