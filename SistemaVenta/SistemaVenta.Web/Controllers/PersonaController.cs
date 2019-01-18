adbb13acd3aa5efa93d3afc72984902760fbbc1d
Irving Herrera <irving.herreramolina@gmail.com>
2019-01-17 19:49:27 -0600

    se creo el controlador y view model de persona, se crearon los metodos de listar cliente, listar provedor y crear cliente

---
+++ SistemaVenta/SistemaVenta.Web/Controllers/PersonaController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SietemaVenta.Entity.Ventas;
+using SistemaVenta.Data;
+using SistemaVenta.Web.Models.Ventas.Persona;
+
+namespace SistemaVenta.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        private readonly DbContextSistema _context;
+
+        public PersonaController(DbContextSistema context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Persona/ListarCliente
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<PersonaViewModel>> ListarCliente()
+        {
+            var persona = await _context.Persona.Where(p => p.TipoPersona.Equals("cliente")).ToListAsync();
+            return persona.Select(p => new PersonaViewModel
+            {
+                IdPersona = p.IdPersona,
+                TipoPersona = p.TipoPersona,
+                Nombre = p.Nombre,
+                TipoDocumento = p.TipoDocumento,
+                NumDocumento = p.NumDocumento,
+                Direccion = p.Direccion,
+                Telefono = p.Telefono,
+                Email = p.Email,
+               
+            });
+        }
+
+        // GET: api/Persona/ListarProvedor
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<PersonaViewModel>> ListarProvedor()
+        {
+            var persona = await _context.Persona.Where(p => p.TipoPersona.Equals("proveedor")).ToListAsync();
+            return persona.Select(p => new PersonaViewModel
+            {
+                IdPersona = p.IdPersona,
+                TipoPersona = p.TipoPersona,
+                Nombre = p.Nombre,
+                TipoDocumento = p.TipoDocumento,
+                NumDocumento = p.NumDocumento,
+                Direccion = p.Direccion,
+                Telefono = p.Telefono,
+                Email = p.Email,
+
+            });
+        }
+
+        // POST: api/Persona/Crear
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Crear([FromBody] PersonaViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var email = model.Email.ToLower();
+
+            if (await _context.Persona.AnyAsync(p => p.Email.Equals(email)))
+            {
+                return BadRequest("El email ya existe");
+            }
+
+            var persona = new Persona
+            {
+                TipoPersona = model.TipoPersona,
+                Nombre = model.Nombre,
+                TipoDocumento = model.TipoDocumento,
+                NumDocumento = model.NumDocumento,
+                Direccion = model.Direccion,
+                Telefono = model.Telefono,
+                Email = model.Email.ToLower(),
+
+            };
+
+            _context.Persona.Add(persona);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok();
+        }
+
+        // PUT: api/Persona/Actualizar
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Actualizar([FromBody] PersonaViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (model.IdPersona <= 0)
+            {
+                return BadRequest();
+            }
+
+            var persona = await _context.Persona
+                            .FirstOrDefaultAsync(p => p.IdPersona == model.IdPersona);
+
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            persona.TipoPersona = model.TipoPersona;
+            persona.Nombre = model.Nombre;
+            persona.TipoDocumento = model.TipoDocumento;
+            persona.NumDocumento = model.NumDocumento;
+            persona.Telefono = model.Telefono;
+            persona.Direccion = model.Direccion;
+            persona.Email = model.Email.ToLower();
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+
+        private bool PersonaExists(int id)
+        {
+            return _context.Persona.Any(e => e.IdPersona == id);
+        }
+    }
+}
---
+++ SistemaVenta/SistemaVenta.Web/Models/Ventas/Persona/PersonaViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVenta.Web.Models.Ventas.Persona
+{
+    public class PersonaViewModel
+    {
+        [Required]
+        public int IdPersona { get; set; }
+        [Required]
+        public string TipoPersona { get; set; }
+        [StringLength(100, MinimumLength = 3,
+            ErrorMessage = "El nombre no debe tener más de 100 caracteres, ni menos de 3 caracteres. ")]
+        public string Nombre { get; set; }
+        public string TipoDocumento { get; set; }
+        public string NumDocumento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
